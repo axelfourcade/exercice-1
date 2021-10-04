73c6a762af074b15f9650fcff1765ea31b9fac38
Axel <Axel@LAPTOP-8DC2K3BE>
2021-10-04 14:32:45 +0200

    création de dépot

---
+++ src/exo1/Exo1.java
@@ -0,0 +1,31 @@
+/*
+ * TPO Exercice 1
+ * Fourcade Axel
+ * 27/09/02
+*/
+package exo1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Axel
+ */
+public class Exo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        System.out.println("Bonjour");
+        System.out.println("Au revoir");
+        
+        String prenom;
+        Scanner sc;
+        sc = new Scanner(System.in);
+        System.out.println("Bonjour, comment vous appelez vous ?");
+        prenom = sc.nextLine();
+    }
+}
